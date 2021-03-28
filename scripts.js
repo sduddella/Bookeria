989e9f24f087abd4e4f71cecd66cba31634a8d6e
sduddella <duddellasiri@gmail.com>
2021-03-28 01:09:12 -0400

    a bunch of stuff

--- scripts.js
+++ scripts.js
@@ -155,16 +155,6 @@
         }
       }
     });
-    /**
-     * Animation on scroll
-     */
-    window.addEventListener('load', () => {
-      AOS.init({
-        duration: 1000,
-        easing: 'ease-in-out',
-        once: true,
-        mirror: false
-      })
-    });
+
   
   })()
